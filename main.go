3910d058ea5e63e8c3d3a666526b9364cabbef9c
bsl <bsh100220@126.com>
2022-03-30 10:08:10 +0800

    参数描述修改

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	flag.StringVar(&patchOutPath, "out", currPath+"/patch"+util.GetNowStr(), "输出目录，默认为当前目录下的[patch+当前时间]目录\n")
 	flag.StringVar(&projectType, "type", "idea", "项目类型，可选(idea,eclipse)，默认为idea")
 	flag.StringVar(&projectConfigFilePath, "conf", "", "idea模块配置文件路径，当type=idea时候有效")
-	svnArgs := flag.String("svn", "", "需要对比的两个版本，用英文冒号(:)分隔，例：100:95，为空时则对比当前目录与svn最新版本")
+	svnArgs := flag.String("svn", "", "需要对比的两个版本，版本号用英文冒号(:)分隔，带有文件路径则用空格分割，例：'100:95'、'100 .'，为空时则对比当前目录与svn最新版本")
 	flag.Parse()
 
 	//解析项目配置文件，得到src、output、WebRoot等路径
